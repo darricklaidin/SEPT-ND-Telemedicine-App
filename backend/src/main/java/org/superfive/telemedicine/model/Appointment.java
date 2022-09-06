2269f72e2755ce3a0cef22d829d5b54cfba11238
darricklaidin <darricklaidin@gmail.com>
2022-09-06 22:27:12 +1000

    Remove Participants Class

--- backend/src/main/java/org/superfive/telemedicine/model/Appointment.java
+++ backend/src/main/java/org/superfive/telemedicine/model/Appointment.java
@@ -7,17 +7,6 @@ import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.util.Calendar;
 
-// Maybe use a class instead of a HashMap to return participants in the appointment
-class Participants {
-    private Doctor doctor;
-    private Patient patient;
-
-    public Participants(Doctor doctor, Patient patient) {
-        this.doctor = doctor;
-        this.patient = patient;
-    }
-}
-
 /**
  * The Appointment entity stores a Calendar instance (date and time) of the appointment, a doctor participant, and a
  * patient participant.
@@ -31,9 +20,9 @@ public class Appointment {
 
     @NotBlank
     private Calendar appointmentSchedule;
+    @NotBlank
     private String appointmentStatus;  // ONGOING, COMPLETED, UPCOMING
 
-    @JsonIgnore
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(
             name = "doctorID",
@@ -42,7 +31,6 @@ public class Appointment {
     )
     private Doctor doctor;
 
-    @JsonIgnore
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(
             name= "patientID",
