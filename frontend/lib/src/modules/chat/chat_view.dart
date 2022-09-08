7f2710d8a871504fa99c9297365afd3788b77837
Nimesh <nchathuranga173@gmail.com>
2022-09-08 15:57:51 +1000

    add base chat ui

--- frontend/lib/src/modules/chat/chat_view.dart
+++ frontend/lib/src/modules/chat/chat_view.dart
@@ -1,3 +1,4 @@
+import 'package:chat_bubbles/chat_bubbles.dart';
 import 'package:flutter/material.dart';
 
 /// Displays chat UI
@@ -24,10 +25,11 @@ class ChatView extends StatelessWidget {
             Text(
               'Dr. Bellamy Nicholas',
               style: TextStyle(
-                  color: Colors.black54,
-                  fontSize: 15,
-                  fontWeight: FontWeight.w300,
-                  fontStyle: FontStyle.italic),
+                color: Colors.black54,
+                fontSize: 15,
+                fontWeight: FontWeight.w300,
+                fontStyle: FontStyle.italic,
+              ),
             ),
           ],
         ),
@@ -76,13 +78,131 @@ class ChatView extends StatelessWidget {
             height: 24,
             color: Colors.black38,
           ),
+          const SizedBox(
+            height: 10,
+          ),
+          BubbleNormal(
+            bubbleRadius: 10,
+            delivered: true,
+            seen: true,
+            text:
+                'hello, doctor, i believe i have the \ncoronavirus as i am experiencing \nmild symtoms, what do i do?',
+            color: const Color.fromARGB(255, 30, 119, 253),
+            tail: true,
+            textStyle: const TextStyle(
+              color: Colors.white,
+              fontSize: 13,
+            ),
+            sent: true,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          BubbleNormal(
+            bubbleRadius: 10,
+            text:
+                'I\'m here for you, don\'t worry. \nWhat symptoms are you \nexperiencing?',
+            color: Colors.blueGrey.shade50,
+            tail: true,
+            textStyle: const TextStyle(
+              color: Colors.black45,
+              fontSize: 13,
+              fontWeight: FontWeight.bold,
+            ),
+            isSender: false,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          BubbleNormal(
+            bubbleRadius: 10,
+            delivered: true,
+            seen: true,
+            text: 'fever\ndry cough\ntiredness\nsore throat',
+            color: const Color.fromARGB(255, 30, 119, 253),
+            tail: true,
+            textStyle: const TextStyle(
+              color: Colors.white,
+              fontSize: 13,
+            ),
+            sent: true,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          BubbleNormal(
+            bubbleRadius: 10,
+            text:
+                'oh so sorry about that. do\nyou have any underlying\ndiseases?',
+            color: Colors.blueGrey.shade50,
+            tail: true,
+            textStyle: const TextStyle(
+              color: Colors.black45,
+              fontSize: 13,
+              fontWeight: FontWeight.bold,
+            ),
+            isSender: false,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          BubbleNormal(
+            bubbleRadius: 10,
+            delivered: true,
+            seen: true,
+            text: 'oh no',
+            color: const Color.fromARGB(255, 30, 119, 253),
+            tail: true,
+            textStyle: const TextStyle(
+              color: Colors.white,
+              fontSize: 13,
+            ),
+            sent: true,
+          ),
           const Spacer(),
-          Row(
-            children: const [
-              Icon(Icons.attach_file),
-              Spacer(),
-              Icon(Icons.send)
-            ],
+          Container(
+            color: Colors.blueGrey.shade50,
+            padding: const EdgeInsets.all(8),
+            child: Row(
+              children: const [
+                Icon(
+                  Icons.attach_file,
+                  size: 35,
+                  color: Colors.grey,
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        contentPadding: EdgeInsets.all(10.0),
+                        constraints: BoxConstraints(maxHeight: 45),
+                        filled: true,
+                        fillColor: Colors.white,
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.transparent, width: 0.0),
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
+                        border: OutlineInputBorder(),
+                        hintText: 'Write a message...',
+                        hintStyle: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 15,
+                          fontWeight: FontWeight.w300,
+                          fontStyle: FontStyle.italic,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Icon(
+                  Icons.send,
+                  size: 35,
+                  color: Colors.grey,
+                )
+              ],
+            ),
           )
         ],
       ),
