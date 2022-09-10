61cb16da1ac90bedea34a5c25b2e421c2419fdd8
darricklaidin <darricklaidin@gmail.com>
2022-09-10 15:52:46 +1000

    Add @Repository annotation to patient repo

--- backend/src/main/java/org/superfive/telemedicine/repository/PatientRepository.java
+++ backend/src/main/java/org/superfive/telemedicine/repository/PatientRepository.java
@@ -3,10 +3,12 @@ package org.superfive.telemedicine.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.superfive.telemedicine.model.Patient;
 
 import java.util.Optional;
 
+@Repository
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
     Page<Patient> findAllBy(Pageable pageable);
 
