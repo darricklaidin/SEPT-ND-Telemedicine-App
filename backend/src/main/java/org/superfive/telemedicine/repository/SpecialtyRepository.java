12ab154d900e05bcc49aa37cefc5fc6f2232cf7f
BryanHongRMIT <s3679989@student.rmit.edu.au>
2022-09-14 00:08:20 +1000

    Specialties Completed

---
+++ backend/src/main/java/org/superfive/telemedicine/repository/SpecialtyRepository.java
@@ -0,0 +1,17 @@
+package org.superfive.telemedicine.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.superfive.telemedicine.model.Specialty;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface SpecialtyRepository extends JpaRepository<Specialty, Integer> {
+    Page<Specialty> findAllBy(Pageable pageable);
+    Optional<Specialty> findBySpecialtyID(int specialtyId);
+    //Created by Java, returns an Optional Object. Hence need to use Optional.
+}
