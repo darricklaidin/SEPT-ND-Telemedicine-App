a6bc691c02aa4a203a09332ae5b1794801f2312c
Nimesh <s3831242@student.rmit.edu.au>
2022-09-16 00:34:07 +1000

    refactor code

--- frontend/lib/modules/chat/chat_screen.dart
+++ frontend/lib/modules/chat/chat_screen.dart
@@ -11,6 +11,7 @@ class ChatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // scroll to latest message on build
     WidgetsBinding.instance.addPostFrameCallback((_) {
       _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
     });
@@ -19,26 +20,7 @@ class ChatScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        title: Row(
-          children: const [
-            CircleAvatar(
-              backgroundImage: NetworkImage(
-                  'https://media.istockphoto.com/photos/portrait-of-a-doctor-picture-id92347250?k=20&m=92347250&s=612x612&w=0&h=RsbWyj485Bf1T6p2GvaZBQhRhGufqKWdRVdO0sdCcSA='),
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            Text(
-              'Dr. Bellamy Nicholas',
-              style: TextStyle(
-                color: Colors.black54,
-                fontSize: 15,
-                fontWeight: FontWeight.w300,
-                fontStyle: FontStyle.italic,
-              ),
-            ),
-          ],
-        ),
+        title: _buildTitleBar(),
         leading: const Icon(
           Icons.arrow_back,
           color: Colors.black,
@@ -47,38 +29,7 @@ class ChatScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              SizedBox(
-                height: 29,
-                child: TextButton(
-                  onPressed: () {},
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all(Colors.red),
-                    padding: MaterialStateProperty.all(
-                      const EdgeInsets.symmetric(horizontal: 25, vertical: 0),
-                    ),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                    ),
-                  ),
-                  child: const Text(
-                    'End Chat',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 11,
-                    ),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                width: 30,
-              )
-            ],
-          ),
+          _buildTopBar(),
           const Divider(
             thickness: 1,
             height: 24,
@@ -88,64 +39,69 @@ class ChatScreen extends StatelessWidget {
           const SizedBox(
             height: 5,
           ),
-          Container(
-            color: Colors.blueGrey.shade50,
-            padding: const EdgeInsets.all(8),
-            child: Row(
-              children: const [
-                Icon(
-                  Icons.attach_file,
-                  size: 35,
-                  color: Colors.grey,
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 8.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        contentPadding: EdgeInsets.all(10.0),
-                        constraints: BoxConstraints(maxHeight: 45),
-                        filled: true,
-                        fillColor: Colors.white,
-                        enabledBorder: OutlineInputBorder(
-                          borderSide:
-                              BorderSide(color: Colors.transparent, width: 0.0),
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                        ),
-                        border: OutlineInputBorder(),
-                        hintText: 'Write a message...',
-                        hintStyle: TextStyle(
-                          color: Colors.black54,
-                          fontSize: 15,
-                          fontWeight: FontWeight.w300,
-                          fontStyle: FontStyle.italic,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Icon(
-                  Icons.send,
-                  size: 35,
-                  color: Colors.grey,
-                )
-              ],
-            ),
-          )
+          _buildBottomBar()
         ],
       ),
     );
   }
 
-  // Widget _buildChatScrollView() {
-  //   return Expanded(
-  //       child: ListView(
-  //     scrollDirection: Axis.vertical,
-  //     shrinkWrap: true,
-  //     reverse: true,
-  //     children: _buildMessages(),
-  //   ));
-  // }
+  Row _buildTitleBar() {
+    return Row(
+      children: const [
+        CircleAvatar(
+          backgroundImage: NetworkImage(
+              'https://media.istockphoto.com/photos/portrait-of-a-doctor-picture-id92347250?k=20&m=92347250&s=612x612&w=0&h=RsbWyj485Bf1T6p2GvaZBQhRhGufqKWdRVdO0sdCcSA='),
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Text(
+          'Dr. Bellamy Nicholas',
+          style: TextStyle(
+            color: Colors.black54,
+            fontSize: 15,
+            fontWeight: FontWeight.w300,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Row _buildTopBar() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        SizedBox(
+          height: 29,
+          child: TextButton(
+            onPressed: () {},
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(Colors.red),
+              padding: MaterialStateProperty.all(
+                const EdgeInsets.symmetric(horizontal: 25, vertical: 0),
+              ),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+            ),
+            child: const Text(
+              'End Chat',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 11,
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(
+          width: 30,
+        )
+      ],
+    );
+  }
 
   Expanded _buildChatScrollView() {
     return Expanded(
@@ -154,9 +110,6 @@ class ChatScreen extends StatelessWidget {
             controller: _scrollController,
             shrinkWrap: true,
             itemCount: messageList.length,
-            // prototypeItem: ListTile(
-            //   title: Text(items.first),
-            // ),
             itemBuilder: (context, index) {
               return Padding(
                 padding: const EdgeInsets.only(top: 20),
@@ -184,27 +137,50 @@ class ChatScreen extends StatelessWidget {
             }));
   }
 
-  // return messageList.map(
-  //   (msg) {
-  //     return Padding(
-  //       padding: const EdgeInsets.only(top: 20),
-  //       child: BubbleNormal(
-  //         bubbleRadius: 10,
-  //         delivered: true,
-  //         seen: true,
-  //         text: msg.text,
-  //         color: msg.isSender
-  //             ? const Color.fromARGB(255, 30, 119, 253)
-  //             : Colors.blueGrey.shade50,
-  //         tail: true,
-  //         textStyle: TextStyle(
-  //             color: msg.isSender ? Colors.white : Colors.black45,
-  //             fontSize: 13,
-  //             fontWeight: msg.isSender ? FontWeight.normal : FontWeight.bold),
-  //         sent: true,
-  //         isSender: msg.isSender,
-  //       ),
-  //     );
-  //   },
-  // ).toList();
+  Container _buildBottomBar() {
+    return Container(
+      color: Colors.blueGrey.shade50,
+      padding: const EdgeInsets.all(8),
+      child: Row(
+        children: const [
+          Icon(
+            Icons.attach_file,
+            size: 35,
+            color: Colors.grey,
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(10.0),
+                  constraints: BoxConstraints(maxHeight: 45),
+                  filled: true,
+                  fillColor: Colors.white,
+                  enabledBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Colors.transparent, width: 0.0),
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                  border: OutlineInputBorder(),
+                  hintText: 'Write a message...',
+                  hintStyle: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w300,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Icon(
+            Icons.send,
+            size: 35,
+            color: Colors.grey,
+          )
+        ],
+      ),
+    );
+  }
 }
