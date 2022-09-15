08ce5d3fc9ce022547b30cdf3a5fccfd3830f7eb
Nimesh <s3831242@student.rmit.edu.au>
2022-09-15 18:17:49 +1000

    style header

--- frontend/lib/modules/chat/chat_screen.dart
+++ frontend/lib/modules/chat/chat_screen.dart
@@ -14,61 +14,76 @@ class ChatScreen extends StatelessWidget {
         elevation: 0,
         title: Row(
           children: const [
-            Icon(
-              Icons.person,
-              color: Colors.grey,
+            CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://media.istockphoto.com/photos/portrait-of-a-doctor-picture-id92347250?k=20&m=92347250&s=612x612&w=0&h=RsbWyj485Bf1T6p2GvaZBQhRhGufqKWdRVdO0sdCcSA='),
+            ),
+            SizedBox(
+              width: 10,
             ),
             Text(
               'Dr. Bellamy Nicholas',
-              style: TextStyle(color: Colors.grey),
+              style: TextStyle(
+                  color: Colors.black54,
+                  fontSize: 15,
+                  fontWeight: FontWeight.w300,
+                  fontStyle: FontStyle.italic),
             ),
           ],
         ),
-        leading: Padding(
-          padding: const EdgeInsets.all(4),
-          child: OutlinedButton(
-            child: const Icon(
-              Icons.arrow_back,
-              color: Colors.black,
-              size: 22,
-            ),
-            onPressed: () {},
-          ),
+        leading: const Icon(
+          Icons.arrow_back,
+          color: Colors.black,
+          size: 20,
         ),
       ),
       body: Column(
         children: [
           Row(
-            // crossAxisAlignment: CrossAxisAlignment.stretch,
-            // mainAxisAlignment: MainAxisAlignment.spaceAround,
-            // crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              // Opacity(
-              //   opacity: 0,
-              //   child: Container(),
-              // ),
-              const Spacer(flex: 6),
-              TextButton(
-                onPressed: () {},
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.red),
-                  // padding: MaterialStateProperty.all<EdgeInsets>(
-                  //   const EdgeInsets.only(right: 20, left: 20),
-                  // ),
-                ),
-                child: const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Text(
+              SizedBox(
+                height: 29,
+                child: TextButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.red),
+                    padding: MaterialStateProperty.all(
+                      const EdgeInsets.symmetric(horizontal: 25, vertical: 0),
+                    ),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                  ),
+                  child: const Text(
                     'End Chat',
                     style: TextStyle(
                       color: Colors.white,
+                      fontSize: 11,
                     ),
                   ),
                 ),
               ),
-              const Spacer(),
+              const SizedBox(
+                width: 30,
+              )
             ],
           ),
+          const Divider(
+            thickness: 1,
+            height: 24,
+            color: Colors.black38,
+          ),
+          const Spacer(),
+          Row(
+            children: const [
+              Icon(Icons.attach_file),
+              Spacer(),
+              Icon(Icons.send)
+            ],
+          )
         ],
       ),
     );
