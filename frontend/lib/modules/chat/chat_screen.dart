a2448b892e9e9f08af5dd57094b84dc6df72b98a
Nimesh <s3831242@student.rmit.edu.au>
2022-09-18 01:04:50 +1000

    create chat room for a hardcoded patient and a doctor

--- frontend/lib/modules/chat/chat_screen.dart
+++ frontend/lib/modules/chat/chat_screen.dart
@@ -3,68 +3,66 @@ import 'package:persistent_bottom_nav_bar/persistent-tab-view.dart';
 import 'package:talkjs_flutter/talkjs_flutter.dart';
 
 import '../home/home_screen.dart';
+import '../../service/user_service.dart';
 
-class ChatScreen extends StatelessWidget {
-  ChatScreen({Key? key}) : super(key: key);
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({Key? key}) : super(key: key);
 
-  final ScrollController _scrollController = ScrollController();
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  late Session _session;
+  late User _me;
+  late User _other;
+  late Conversation _conversation;
+  bool isPatient = false;
+
+  Future? _myFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _myFuture = _createChatRoom();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // create a TalkJS session
-    final session = Session(appId: 'taq5zIcQ');
+    return FutureBuilder(
+        future: _myFuture,
+        builder: (context, snapshot) {
+          Widget body;
 
-    // create a TalkJS user
-    final me = session.getUser(
-      id: '123456',
-      name: 'Alice',
-      email: ['alice@example.com'],
-      photoUrl: 'https://talkjs.com/images/avatar-1.jpg',
-      welcomeMessage: 'Hey there! How are you? :-)',
-      role: 'default',
-    );
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            body = const Center(child: CircularProgressIndicator());
+          } else {
+            if (snapshot.hasError) {
+              body = Center(child: Text('Error: ${snapshot.error}'));
+            } else {
+              body = ChatBox(
+                session: _session,
+                conversation: _conversation,
+              );
+            } // snapshot.data  :- get your object which is pass from your downloadData() function
+          }
 
-    // set the active TalkJS user to the session
-    session.me = me;
-
-    // create another user to create a conversation with
-    final other = session.getUser(
-      id: '654321',
-      name: 'Sebastian',
-      email: ['Sebastian@example.com'],
-      photoUrl: 'https://talkjs.com/images/avatar-5.jpg',
-      welcomeMessage: 'Hey, how can I help?',
-      role: 'default',
-    );
-
-    // create chat
-    final conversation = session.getConversation(
-      id: Talk.oneOnOneId(me.id, other.id),
-      participants: {Participant(me), Participant(other)},
-    );
-
-    // scroll to latest message on build
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      _scrollController.jumpTo(_scrollController.position.maxScrollExtent);
-    });
-
-    return Scaffold(
-      appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          title: _buildTitleBar(),
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back, size: 20, color: Colors.black),
-            onPressed: () => pushNewScreen(
-              context,
-              screen: const HomeScreen(),
-            ),
-          )),
-      body: ChatBox(
-        session: session,
-        conversation: conversation,
-      ),
-    );
+          return Scaffold(
+            appBar: AppBar(
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+                title: _buildTitleBar(),
+                leading: IconButton(
+                  icon: const Icon(Icons.arrow_back,
+                      size: 20, color: Colors.black),
+                  onPressed: () => pushNewScreen(
+                    context,
+                    screen: const HomeScreen(),
+                  ),
+                )),
+            body: body,
+          );
+        });
   }
 
   Row _buildTitleBar() {
@@ -82,6 +80,39 @@ class ChatScreen extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+
+  Future<void> _createChatRoom() async {
+    // create a TalkJS session
+    _session = Session(appId: 'taq5zIcQ');
+
+    // create a TalkJS user
+    final me = isPatient ? await getPatient() : await getDoctor();
+    _me = _session.getUser(
+      id: '${(isPatient ? 'p' : 'd')}${me.id.toString()}', // create unique depending on user type
+      name: '${me.firstName} ${me.lastName}',
+      email: [me.email],
+      photoUrl: 'https://talkjs.com/images/avatar-1.jpg',
+    );
+
+    // set the active TalkJS user to the session
+    _session.me = _me;
+
+    // create another user to create a conversation with
+    final other = isPatient ? await getDoctor() : await getPatient();
+
+    _other = _session.getUser(
+      id: '${(isPatient ? 'p' : 'd')}${other.id.toString()}',
+      name: '${other.firstName} ${other.lastName}',
+      email: [other.email],
+      photoUrl: 'https://talkjs.com/images/avatar-1.jpg',
+    );
+
+    // create chat
+    _conversation = _session.getConversation(
+      id: Talk.oneOnOneId(_me.id, _other.id),
+      participants: {Participant(_me), Participant(_other)},
     );
   }
 }
