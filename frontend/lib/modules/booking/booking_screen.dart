de9e9e6d3e94e514b46530efed70b4e4c5170828
darricklaidin <darricklaidin@gmail.com>
2022-09-19 23:31:11 +1000

    Start work on book appointment frontend logic

--- frontend/lib/modules/booking/booking_screen.dart
+++ frontend/lib/modules/booking/booking_screen.dart
@@ -3,6 +3,8 @@ import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 import 'package:time_picker_widget/time_picker_widget.dart';
 
 class BookingScreen extends StatefulWidget {
+  // Doctor doctor;
+
   const BookingScreen({super.key});
 
   @override
@@ -12,6 +14,8 @@ class BookingScreen extends StatefulWidget {
 class _BookingScreenState extends State<BookingScreen> {
   int _index = 0;
 
+  // TODO: Dynamically generate these depending on doctor's availability and user appointments
+
   final List<DateTime> UNAVAILABE_DATES = [
     DateTime(2022, 09, 20),
     DateTime(2022, 09, 21)
@@ -19,39 +23,82 @@ class _BookingScreenState extends State<BookingScreen> {
 
   // list the available hours from the backend as INT values in 24 hr format
   final List<int> _availableHours = [4, 5, 6, 7, 10, 11, 13, 14, 16, 19];
-
   // hardcode this list(from this you can get the slots hourly)
   final List<int> _availableMinutes = [0];
 
   // selected time - in 12 hr format
-  var selectedTime;
+  TimeOfDay? selectedStartTime;
+  TimeOfDay? selectedEndTime;
+
+  var selectedDate;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Stepper(
-            currentStep: _index,
-            onStepCancel: () {
-              if (_index > 0) {
-                setState(() {
-                  _index -= 1;
-                });
-              }
-            },
-            onStepContinue: () {
-              if (_index <= 1) {
-                // logic for checking valid date
-                setState(() {
-                  _index += 1;
-                });
-              }
-            },
-            onStepTapped: (int index) {
+          currentStep: _index,
+          onStepCancel: () {
+            if (_index > 0) {
               setState(() {
-                _index = index;
+                _index -= 1;
               });
-            },
-            steps: _buildSteps()));
+            }
+          },
+          onStepContinue: () {
+            if (_index <= 2) {
+              // logic for checking valid date
+              setState(() {
+                _index += 1;
+              });
+            }
+          },
+          onStepTapped: (int index) {
+            setState(() {
+              _index = index;
+            });
+          },
+          steps: _buildSteps(),
+          controlsBuilder: (context, _) {
+            if (_index == 3) {
+              return Row(
+                children: <Widget>[
+                  Container(),
+                  TextButton(
+                    onPressed: _.onStepCancel,
+                    child: const Text('BACK'),
+                  ),
+                ],
+              );
+            }
+            else if (_index == 0) {
+              return Row(
+                children: <Widget>[
+                  TextButton(
+                    onPressed: _.onStepContinue,
+                    child: const Text('NEXT'),
+                  ),
+                  TextButton(
+                    onPressed: Navigator.of(context).pop,
+                    child: const Text('CANCEL'),
+                  ),
+                ],
+              );
+            }
+            return Row(
+              children: <Widget>[
+                TextButton(
+                  onPressed: _.onStepContinue,
+                  child: const Text('NEXT'),
+                ),
+                TextButton(
+                  onPressed: _.onStepCancel,
+                  child: const Text('BACK'),
+                ),
+              ],
+            );
+          }
+        )
+    );
   }
 
   List<Step> _buildSteps() {
@@ -60,33 +107,84 @@ class _BookingScreenState extends State<BookingScreen> {
           title: const Text('Select Date'),
           content: SfDateRangePicker(
             minDate: DateTime.now(),
-            monthViewSettings: DateRangePickerMonthViewSettings(
-                blackoutDates: UNAVAILABE_DATES),
-          )),
+            monthViewSettings: DateRangePickerMonthViewSettings(blackoutDates: UNAVAILABE_DATES),
+            selectionMode: DateRangePickerSelectionMode.single,
+            monthCellStyle: const DateRangePickerMonthCellStyle(
+              blackoutDateTextStyle: TextStyle(color: Colors.grey),
+            ),
+            showNavigationArrow: true,
+            showTodayButton: true,
+            onSelectionChanged: (DateRangePickerSelectionChangedArgs args) {
+              print(args.value);
+              setState(() {
+                selectedDate = args.value;
+              });
+            },
+          )
+      ),
       Step(
-          title: Text('Select time'),
+          title: Text('Select start time'),
           content: Center(
               child: TextButton(
-            onPressed: () => showCustomTimePicker(
-                context: context,
-                onFailValidation: (context) =>
-                    Text('this time slot is already booked'),
-                //for the initial time you must set a value inside the _availableHours list
-                initialTime: TimeOfDay(hour: 14, minute: 0),
-                selectableTimePredicate: (time) =>
-                    _availableHours.indexOf(time!.hour) != -1 &&
-                    _availableMinutes.indexOf(time.minute) != -1).then(
-                (time) => setState(() => selectedTime = time?.format(context))),
-            child: Text(
-              selectedTime ?? 'Select Time',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
-            ),
-          ))),
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all<EdgeInsets>(const EdgeInsets.all(10)),
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.amber),
+                  foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                ),
+                onPressed: () {
+                  showCustomTimePicker(
+                    context: context,
+                    onFailValidation: (context) => const Text('This time slot is already booked'),
+                    // For the initial time you must set a value inside the _availableHours list
+                    initialTime: TimeOfDay(hour: 14, minute: 0),
+                    selectableTimePredicate: (time) => true,
+                  ).then((time) => setState(() {
+                    selectedStartTime = time;
+                    print(selectedStartTime);
+                  }));
+                },
+                child: Text(
+                  selectedStartTime?.format(context) ?? 'Select start time',
+                  style: const TextStyle(fontSize: 16),
+                ),
+          )
+        )
+      ),
+      Step(
+          title: Text('Select end time'),
+          content: Center(
+              child: TextButton(
+                style: ButtonStyle(
+                  padding: MaterialStateProperty.all<EdgeInsets>(const EdgeInsets.all(10)),
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.amber),
+                  foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                ),
+                onPressed: () {
+                  showCustomTimePicker(
+                    context: context,
+                    onFailValidation: (context) => const Text('This time slot is already booked'),
+                    // For the initial time you must set a value inside the _availableHours list
+                    initialTime: TimeOfDay(hour: 14, minute: 0),
+                    selectableTimePredicate: (time) => true,
+                  ).then((time) => setState(() {
+                    selectedEndTime = time;
+                    print(selectedEndTime);
+                  }));
+                },
+                child: Text(
+                  selectedEndTime?.format(context) ?? 'Select end time',
+                  style: const TextStyle(fontSize: 16),
+                ),
+              )
+          )
+      ),
       Step(
         title: const Text('Confirm'),
         content: TextButton(
           onPressed: () {
-            Navigator.pushNamed(context, '/');
+            // TODO: Make appointment in backend
+
+            // Navigator.of(context).pop();
           },
           style: ButtonStyle(
             backgroundColor: MaterialStateProperty.all(
