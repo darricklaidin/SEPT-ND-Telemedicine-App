f3dcb21979128f8f31bf8cac38dee90d4bfdd12f
BryanHongRMIT <s3679989@student.rmit.edu.au>
2022-09-20 20:50:54 +1000

    updated pom Mockito and imports

--- backend/auth-microservice/src/test/java/com/sept/authmicroservice/service/PatientServiceTest.java
+++ backend/auth-microservice/src/test/java/com/sept/authmicroservice/service/PatientServiceTest.java
@@ -1,9 +1,36 @@
 package com.sept.authmicroservice.service;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.BDDMockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.RepeatedTest;
+import com.sept.authmicroservice.exception.ResourceAlreadyExistsException;
+import com.sept.authmicroservice.exception.ResourceNotFoundException;
+import com.sept.authmicroservice.model.*;
+import com.sept.authmicroservice.service.*;
+import com.sept.authmicroservice.repository.PatientRepository;
+import com.sept.authmicroservice.service.PatientService;
+
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.*;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class PatientServiceTest {
 
