86ac44e7b07f88a99efc56ed9098035a4d08b6e9
darricklaidin <darricklaidin@gmail.com>
2022-09-29 01:58:23 +1000

    Update theme secondary color in light_palette.dart

--- frontend/lib/config/themes/light_palette.dart
+++ frontend/lib/config/themes/light_palette.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 class LightPalette {
   static const Color primary = Color(0xFFC39BEE);
-  static const Color secondary = Color(0xFFFDDA44);
+  static const Color secondary = Colors.amber;
   static const Color error = Colors.deepOrange;
 }
