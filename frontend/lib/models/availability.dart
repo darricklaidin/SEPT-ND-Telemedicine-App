6e55393cdb12081260fb58f13b4d4974586a2e86
darricklaidin <darricklaidin@gmail.com>
2022-09-29 18:07:20 +1000

    Update availability constructor

--- frontend/lib/models/availability.dart
+++ frontend/lib/models/availability.dart
@@ -4,30 +4,30 @@ import 'package:frontend/models/doctor.dart';
 import 'package:frontend/utility.dart';
 
 class Availability {
-  int? availabilityID;
-  int? dayOfWeek;
-  TimeOfDay? startTime;
-  TimeOfDay? endTime;
-  Doctor? doctor;
+  int availabilityID;
+  int dayOfWeek;
+  TimeOfDay startTime;
+  TimeOfDay endTime;
+  Doctor doctor;
 
   Availability({
-    this.availabilityID,
-    this.dayOfWeek,
-    this.startTime,
-    this.endTime,
-    this.doctor,
+    required this.availabilityID,
+    required this.dayOfWeek,
+    required this.startTime,
+    required this.endTime,
+    required this.doctor,
   });
 
-  factory Availability.fromJson(Map<String, dynamic> json) {
+  factory Availability.fromJson(Map<String, dynamic> json, Doctor doctor) {
     return Availability(
       availabilityID: json['availabilityID'],
       dayOfWeek: Utility.convertDayOfWeekToInt(json['dayOfWeek']),
-      startTime: TimeOfDay(hour: int.parse(json['startTime'].split(':')[0]), minute: int.parse(json['startTime'].split(':')[1])),
-      endTime: TimeOfDay(hour: int.parse(json['endTime'].split(':')[0]), minute: int.parse(json['endTime'].split(':')[1])),
-      doctor: Doctor.fromJson(json['doctor']),
+      startTime: TimeOfDay(hour: int.parse(json['startTime'].split(':')[0]),
+          minute: int.parse(json['startTime'].split(':')[1])),
+      endTime: TimeOfDay(hour: int.parse(json['endTime'].split(':')[0]),
+          minute: int.parse(json['endTime'].split(':')[1])),
+      doctor: doctor,
     );
   }
-
-
 
 }
