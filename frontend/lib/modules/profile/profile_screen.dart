84f56879b286a524e96fa19f305cb0eb469c2a24
darricklaidin <darricklaidin@gmail.com>
2022-09-29 18:08:59 +1000

    Implement show availabilities in doctor user profile

--- frontend/lib/modules/profile/profile_screen.dart
+++ frontend/lib/modules/profile/profile_screen.dart
@@ -1,7 +1,14 @@
+import 'dart:async';
+
 import 'package:age_calculator/age_calculator.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend/modules/profile/profile_button.dart';
+import 'package:frontend/services/doctor_service.dart';
+import 'package:frontend/utility.dart';
+
+import '../../services/patient_service.dart';
+import '../appointment/book_appointment_screen.dart';
 
 class ProfileScreen extends StatefulWidget {
 
@@ -16,18 +23,74 @@ class ProfileScreen extends StatefulWidget {
 
 class _ProfileScreenState extends State<ProfileScreen> {
 
+  List availabilitySchedule = [];
+  bool isLoading = true;
+  bool timeUp = false;
+
+  Future loadAvailabilities() async {
+    isLoading = true;
+    timeUp = false;
+
+    try {
+      availabilitySchedule = await DoctorService
+          .fetchDoctorAvailabilities(widget.user.userID);
+    } on TimeoutException {
+      setState(() {
+        timeUp = true;
+        isLoading = false;
+      });
+      return;
+    } on Exception catch (exception) {
+      print(exception);
+    }
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
+  Future checkIfUserExists(Color errorThemeColor) async {
+    if (widget.userRole == "PATIENT" ?
+    await DoctorService.fetchDoctor(widget.user.userID) == "Resource Not Found"
+        : await PatientService.fetchPatient(widget.user.userID) == "Resource Not Found") {
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          behavior: SnackBarBehavior.floating,
+          margin: const EdgeInsets.only(bottom: 10.0),
+          duration: const Duration(seconds: 2),
+          content: const Text('User no longer exists'),
+          backgroundColor: errorThemeColor,
+        ),
+      );
+
+      Navigator.pop(context);
+      return false;
+    }
+
+    return true;
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadAvailabilities();
+  }
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
     Color primaryThemeColor = Theme.of(context).colorScheme.primary;
     Color secondaryThemeColor = Theme.of(context).colorScheme.secondary;
+    Color errorThemeColor = Theme.of(context).colorScheme.error;
 
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        title: _buildTitleRow(),
+        title: _buildTitleRow(width, height, primaryThemeColor, secondaryThemeColor),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
           color: Colors.black,
@@ -36,24 +99,47 @@ class _ProfileScreenState extends State<ProfileScreen> {
           },
         ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ..._buildProfileHeader(width, height, primaryThemeColor, secondaryThemeColor),
-          SizedBox(height: height * 0.05),
-          ..._buildBookAppointmentBtn(width, height, primaryThemeColor, secondaryThemeColor),
-        ],
+      body: Builder(
+        builder: (context) {
+          if (isLoading) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            return SizedBox(
+              height: height,
+              child: RefreshIndicator(
+                onRefresh: () async {
+                  if (await checkIfUserExists(errorThemeColor)) {
+                    await loadAvailabilities();
+                  }
+                },
+                child: SingleChildScrollView(
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ..._buildProfileHeader(width, height, primaryThemeColor, secondaryThemeColor),
+                      SizedBox(height: height * 0.01),
+                      ..._buildAvailabilitySchedule(width, height, primaryThemeColor, secondaryThemeColor, errorThemeColor),
+                      SizedBox(height: height * 0.05),
+                      ..._buildBookAppointmentBtn(width, height, primaryThemeColor, secondaryThemeColor, errorThemeColor),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          }
+        },
       ),
     );
   }
 
-  Row _buildTitleRow() {
+  Row _buildTitleRow(double width, double height, Color primaryThemeColor, Color secondaryThemeColor) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.end,
-      children: const [
-        ProfileButton(),
+      children: [
+        const ProfileButton(),
         SizedBox(
-          width: 20,
+          width: width * 0.05,
         )
       ],
     );
@@ -64,8 +150,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
       const Center(
         child: Icon(CupertinoIcons.profile_circled, size: 100),
       ),
-      const SizedBox(
-        height: 20,
+      SizedBox(
+        height: height * 0.05,
       ),
       Text(
         '${widget.user.firstName} ${widget.user.lastName}',
@@ -74,25 +160,73 @@ class _ProfileScreenState extends State<ProfileScreen> {
       ),
       Text(
         widget.userRole == "PATIENT" ? '${widget.user.specialty}' : 'Age: ${AgeCalculator.age(widget.user.dateOfBirth).years}',
-        style: const TextStyle(fontSize: 18, color: Colors.grey),
+        style: const TextStyle(fontSize: 14, color: Colors.grey),
         textAlign: TextAlign.center,
       ),
-      const SizedBox(
-        height: 10,
-      )
     ];
   }
 
-  List<Widget> _buildBookAppointmentBtn(double width, double height, Color primaryThemeColor, Color secondaryThemeColor) {
+  List<Widget> _buildAvailabilitySchedule(double width, double height, Color primaryThemeColor, Color secondaryThemeColor, Color errorThemeColor) {
+    if (widget.userRole == "PATIENT") {
+      if (timeUp) {
+        return [
+          SizedBox(
+            child: Container(
+              padding: EdgeInsets.fromLTRB(0, height * 0.05, 0, 0),
+              child: const Text("Timeout: Unable to fetch the doctor's availabilities"),
+            ),
+        ),
+        ];
+      }
+      else if (availabilitySchedule.isEmpty) {
+        return [
+          SizedBox(
+            child: Container(
+              padding: EdgeInsets.fromLTRB(0, height * 0.05, 0, 0),
+              child: const Text("No availabilities found"),
+            ),
+          ),
+        ];
+      }
+
+      return [
+        ListView.separated(
+          itemCount: availabilitySchedule.length,
+          itemBuilder: (context, index) {
+            return Container(
+              padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
+              height: height * 0.1,
+              child: ListTile(
+                contentPadding: EdgeInsets.symmetric(horizontal: width * 0.1, vertical: 0),
+                title: Text(Utility.convertIntToDayOfWeek(availabilitySchedule[index].dayOfWeek), style: const TextStyle(fontSize: 14),),
+                subtitle: Text("${Utility.timeToString(availabilitySchedule[index].startTime)} "
+                    "- ${Utility.timeToString(availabilitySchedule[index].endTime)}", style: const TextStyle(fontSize: 12)),
+              ),
+            );
+          },
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          separatorBuilder: (context, index) {
+            return const SizedBox(height: 0);
+          },
+        ),
+      ];
+    }
+    return [SizedBox(height: height * 0.05)];
+  }
+
+  List<Widget> _buildBookAppointmentBtn(double width, double height, Color primaryThemeColor, Color secondaryThemeColor, Color errorThemeColor) {
     return widget.userRole == "DOCTOR" ? [SizedBox(height: height * 0.05,)] : [
       Center(
         child: SizedBox(
           width: width * 0.45,
           height: 40,
           child: TextButton(
-            // TODO: Navigate to make appointment page
-            onPressed: () {
-
+            // Navigate to make appointment page
+            onPressed: () async {
+              if (await checkIfUserExists(errorThemeColor)) {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const BookAppointmentScreen()));
+              }
             },
             style: ButtonStyle(
               backgroundColor: MaterialStateProperty.all(secondaryThemeColor),
