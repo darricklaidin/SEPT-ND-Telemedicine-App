e3d128d994637825a068eaae35cc37fdc40558dd
darricklaidin <darricklaidin@gmail.com>
2022-10-01 08:36:15 +1000

    Create edit availability screen skeleton

---
+++ frontend/lib/modules/doctor/edit_availability_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:interval_time_picker/interval_time_picker.dart';
+
+class EditAvailabilityScreen extends StatefulWidget {
+  const EditAvailabilityScreen({Key? key}) : super(key: key);
+
+  @override
+  State<EditAvailabilityScreen> createState() => _EditAvailabilityScreenState();
+}
+
+class _EditAvailabilityScreenState extends State<EditAvailabilityScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
