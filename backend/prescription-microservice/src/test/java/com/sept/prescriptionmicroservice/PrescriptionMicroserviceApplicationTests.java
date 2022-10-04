365c3060fa36dbefea8c44551bd5b70cd32629ec
BryanHongRMIT <s3679989@student.rmit.edu.au>
2022-10-04 23:57:38 +1100

    microservice prescriptions

---
+++ backend/prescription-microservice/src/test/java/com/sept/prescriptionmicroservice/PrescriptionMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sept.prescriptionmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrescriptionMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
