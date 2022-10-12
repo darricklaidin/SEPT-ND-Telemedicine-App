97ade44d22fe7e13a14b6f72b106d2066424ecaa
darricklaidin <darricklaidin@gmail.com>
2022-10-12 16:00:30 +1100

    Complete specialty service unit test

--- backend/auth-microservice/src/test/java/com/sept/authmicroservice/unit_test/SpecialtyServiceUnitTest.java
+++ backend/auth-microservice/src/test/java/com/sept/authmicroservice/unit_test/SpecialtyServiceUnitTest.java
@@ -1,126 +1,164 @@
 package com.sept.authmicroservice.unit_test;
 
-import com.sept.authmicroservice.repository.DoctorRepository;
-import com.sept.authmicroservice.repository.UserRepository;
-import com.sept.authmicroservice.service.DoctorService;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.mockito.BDDMockito;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Assertions;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-import com.sept.authmicroservice.exception.ResourceAlreadyExistsException;
-import com.sept.authmicroservice.model.*;
+import com.sept.authmicroservice.model.Specialty;
+import com.sept.authmicroservice.payload.SpecialtyDTO;
 import com.sept.authmicroservice.repository.SpecialtyRepository;
 import com.sept.authmicroservice.service.SpecialtyService;
-import com.sept.authmicroservice.payload.SpecialtyDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 
-import java.awt.print.Pageable;
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 
 class SpecialtyServiceUnitTest {
 
-    private int specialtyID;
-    private String specialtyName;
-    private Set<Doctor> doctors;
     private SpecialtyService specialtyService;
-    private SpecialtyDTO specialtyDTO;
-    private Specialty specialty;
+    private SpecialtyRepository mockSpecialtyRepository;
+
+    private Specialty specialty1;
     private Specialty specialty2;
 
-    private SpecialtyRepository mockSpecialtyRepository;
+    private final List<Specialty> specialties = new ArrayList<>();
 
     @BeforeEach
     void setUp() {
-
         mockSpecialtyRepository = Mockito.mock(SpecialtyRepository.class);
-
         specialtyService = new SpecialtyService(mockSpecialtyRepository);
 
-        specialty = new Specialty("Surgery");
+        // Create new specialties
+        specialty1 = new Specialty("Surgery");
+        specialty1.setSpecialtyID(1);
         specialty2 = new Specialty("Optometry");
+        specialty2.setSpecialtyID(2);
+
+        // Add specialties to specialties list
+        specialties.add(specialty1);
+        specialties.add(specialty2);
     }
 
     @Test
     void getAllSpecialties() {
-
         when(mockSpecialtyRepository.findAllBy(null))
-                .thenReturn(new PageImpl<>(new ArrayList<>(Arrays.asList(specialty, specialty2))));
+                .thenReturn(new PageImpl<>(new ArrayList<>(Arrays.asList(specialty1, specialty2))));
 
-        Page<Specialty> specialtyPage = specialtyService.getAllSpecialties(null);  // Get doctor page
-
-        List<Specialty> specialties = specialtyPage.getContent();  // Get list of doctors from doctor page
+        Page<Specialty> specialtyPage = specialtyService.getAllSpecialties(null);
+        List<Specialty> retrievedSpecialties = specialtyPage.getContent();
 
         // Test specialty list length
-        assertEquals(2, specialties.size());
+        assertEquals(specialties.size(), retrievedSpecialties.size());
 
-        // Test specialty match
-        assertEquals(specialty, specialties.get(0));
-
-        // Test specialty2 match
-        assertEquals(specialty2, specialties.get(1));
+        // Test each specialty matches
+        for (int i = 0; i < specialties.size(); i++) {
+            assertEquals(specialties.get(i), retrievedSpecialties.get(i));
+        }
     }
 
     @Test
     void getSpecialtyByID() {
+        when(mockSpecialtyRepository.findBySpecialtyID(specialty1.getSpecialtyID())).thenReturn(Optional.of(specialty1));
 
-        when(mockSpecialtyRepository.findBySpecialtyID(specialtyID)).thenReturn(Optional.of(specialty));
-        Assertions.assertTrue(specialtyService.getSpecialtyByID(specialtyID).equals(specialty));
+        // Test specialty1 matches
+        assertEquals(specialtyService.getSpecialtyByID(specialty1.getSpecialtyID()), specialty1);
     }
 
     @Test
     void getSpecialtyByName() {
-        when(mockSpecialtyRepository.findBySpecialtyName(specialtyName)).thenReturn(Optional.of(specialty));
-        Assertions.assertTrue(specialtyService.getSpecialtyByName(specialtyName).equals(specialty));
+        when(mockSpecialtyRepository.findBySpecialtyName(specialty1.getSpecialtyName())).thenReturn(Optional.of(specialty1));
+
+        // Test specialty1 matches
+        assertEquals(specialtyService.getSpecialtyByName(specialty1.getSpecialtyName()), specialty1);
     }
 
     @Test
     void createSpecialty() {
+        SpecialtyDTO specialty1DTO = new SpecialtyDTO();
+        specialty1DTO.setSpecialtyName("Cardiology");
 
-        BDDMockito.given(mockSpecialtyRepository.findBySpecialtyID(specialty.getSpecialtyID())).willReturn(Optional.of(specialty));
+        Specialty newSpecialty = new Specialty(specialty1DTO.getSpecialtyName());
 
-        SpecialtyDTO specialtyDTO1 = new SpecialtyDTO();
+        when(mockSpecialtyRepository.save(newSpecialty)).thenReturn(newSpecialty);
 
-        specialtyDTO1.setSpecialtyName("Cardiology");
+        Specialty createdSpecialty = specialtyService.createSpecialty(specialty1DTO);
 
-        System.out.println(specialtyDTO1.getSpecialtyName());
+        when(mockSpecialtyRepository.findBySpecialtyID(createdSpecialty.getSpecialtyID()))
+                .thenReturn(Optional.of(createdSpecialty));
 
-        Specialty tester = specialtyService.createSpecialty(specialtyDTO1);
+        when(mockSpecialtyRepository.findAllBy(null))
+                .thenReturn(new PageImpl<>(new ArrayList<>(Arrays.asList(specialty1, specialty2, createdSpecialty))));
 
-        verify(mockSpecialtyRepository).save(tester);
+        Page<Specialty> specialtyPage = specialtyService.getAllSpecialties(null);
+        List<Specialty> retrievedSpecialties = specialtyPage.getContent();
+
+        // Test specialty list length
+        assertEquals(specialties.size() + 1, retrievedSpecialties.size());
+
+        // Test get newSpecialty by ID
+        assertEquals(createdSpecialty, specialtyService.getSpecialtyByID(createdSpecialty.getSpecialtyID()));
+
+        // Test newSpecialty name matches
+        assertEquals(newSpecialty.getSpecialtyName(), retrievedSpecialties.get(specialties.size()).getSpecialtyName());
 
     }
 
 
     @Test
     void updateSpecialty() {
-
         SpecialtyDTO specialtyDTO1 = new SpecialtyDTO();
+        specialtyDTO1.setSpecialtyName("Childcare");
 
-        specialtyDTO1.setSpecialtyName("Child");
+        Specialty newSpecialty = new Specialty(specialtyDTO1.getSpecialtyName());
 
-        BDDMockito.given(mockSpecialtyRepository.findBySpecialtyID(specialty.getSpecialtyID())).willReturn(Optional.of(specialty));
+        when(mockSpecialtyRepository.save(newSpecialty)).thenAnswer(i -> {
+            specialty1.setSpecialtyName(newSpecialty.getSpecialtyName());
+            return specialty1;
+        });
 
-        specialtyService.updateSpecialty(specialty.getSpecialtyID(), specialtyDTO1);
+        when(mockSpecialtyRepository.findBySpecialtyID(specialty1.getSpecialtyID()))
+                .thenReturn(Optional.of(newSpecialty));
 
-        assertEquals("Child", specialty.getSpecialtyName());
+        Specialty updatedSpecialty = specialtyService.updateSpecialty(specialty1.getSpecialtyID(), specialtyDTO1);
+
+        when(mockSpecialtyRepository.findAllBy(null))
+                .thenReturn(new PageImpl<>(new ArrayList<>(Arrays.asList(specialty1, specialty2))));
+
+        // Test that specialty1's name has been updated
+        assertEquals("Childcare", specialty1.getSpecialtyName());
+
+        // Test that the id of specialty 1 remains unchanged
+        assertEquals(1, specialty1.getSpecialtyID());
+
+        // Test that updatedSpecialty matches with specialty1
+        assertEquals(specialty1, updatedSpecialty);
 
     }
 
     @Test
     void deleteSpecialty() {
-        when(mockSpecialtyRepository.findBySpecialtyID(specialty.getSpecialtyID())).thenReturn(Optional.of(specialty));
+        when(mockSpecialtyRepository.findBySpecialtyID(specialty1.getSpecialtyID())).thenReturn(Optional.of(specialty1));
 
-        specialtyService.deleteSpecialty(specialty.getSpecialtyID());
+        doNothing().when(mockSpecialtyRepository).deleteById(specialty1.getSpecialtyID());
 
-        verify(mockSpecialtyRepository).deleteById(specialty.getSpecialtyID());
+        Specialty deletedSpecialty = specialtyService.deleteSpecialty(specialty1.getSpecialtyID());
+
+        when(mockSpecialtyRepository.findAllBy(null))
+                .thenReturn(new PageImpl<>(new ArrayList<>(Collections.singletonList(specialty2))));
+
+        Page<Specialty> specialtyPage = specialtyService.getAllSpecialties(null);
+        List<Specialty> retrievedSpecialties = specialtyPage.getContent();
+
+        // Test specialty list length
+        assertEquals(specialties.size() - 1, retrievedSpecialties.size());
+
+        // Test that specialty1 has been deleted
+        assertEquals(specialty2, retrievedSpecialties.get(0));
+
+        // Test that deleted specialty matches specialty1
+        assertEquals(specialty1, deletedSpecialty);
     }
 }
