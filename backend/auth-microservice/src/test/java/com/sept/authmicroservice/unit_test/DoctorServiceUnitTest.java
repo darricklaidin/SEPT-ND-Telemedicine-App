26d6a2a17134caaafa960c0bb63496bc54155402
darricklaidin <darricklaidin@gmail.com>
2022-10-12 16:21:11 +1100

    Complete doctor service unit test

--- backend/auth-microservice/src/test/java/com/sept/authmicroservice/unit_test/DoctorServiceUnitTest.java
+++ backend/auth-microservice/src/test/java/com/sept/authmicroservice/unit_test/DoctorServiceUnitTest.java
@@ -1,137 +1,142 @@
 package com.sept.authmicroservice.unit_test;
 
+import com.sept.authmicroservice.model.Doctor;
 import com.sept.authmicroservice.repository.DoctorRepository;
 import com.sept.authmicroservice.repository.UserRepository;
 import com.sept.authmicroservice.service.DoctorService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.mockito.BDDMockito;
-import org.junit.jupiter.api.BeforeEach;
-import com.sept.authmicroservice.model.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 
 class DoctorServiceUnitTest {
 
     private DoctorRepository mockDoctorRepository;
-    private UserRepository mockUserRepository;
     private DoctorService doctorService;
 
-    private Doctor doctor;
+    private Doctor doctor1;
     private Doctor doctor2;
-    private Doctor doctor3;
-    private Doctor doctor4;
+    private final List<Doctor> doctors = new ArrayList<>();
 
-    LocalDate dob;
+    LocalDate dob1;
     LocalDate dob2;
-    LocalDate dob3;
-    LocalDate dob4;
 
 
     @BeforeEach
     void setUp() {
         mockDoctorRepository = Mockito.mock(DoctorRepository.class);
-        mockUserRepository = Mockito.mock(UserRepository.class);
-
+        UserRepository mockUserRepository = Mockito.mock(UserRepository.class);
         doctorService = new DoctorService(mockDoctorRepository, mockUserRepository);
 
-        dob = LocalDate.of(2000, Month.JANUARY, 1);
+        dob1 = LocalDate.of(2000, Month.JANUARY, 1);
         dob2 = LocalDate.of(2001, Month.FEBRUARY, 2);
-        dob3 = LocalDate.of(2002, Month.MARCH, 3);
-        dob4 = LocalDate.of(2003, Month.APRIL, 4);
 
-        doctor = new Doctor(1, "Hirday", "Bajaj", "patient@fmail.com", "password", dob, null , null);
+        doctor1 = new Doctor(1, "Hirday", "Bajaj", "patient@fmail.com", "password", dob1, null , null);
         doctor2 = new Doctor(2, "Mohamed", "Mahamed", "pc@fmail.coa", "password", dob2, null , null);
-        doctor3 = new Doctor(3, "Bryan", "Hong", "update@gmail.com", "update", dob3 , null, null);
-        doctor4 = new Doctor(4, "Nim", "W", "update@gmail.com", "update", dob4, null,null);
 
+        doctors.add(doctor1);
+        doctors.add(doctor2);
     }
 
-    /*
-     *  For now, just have a few test cases for each method. Testing should be a priority only once we are satisfied
-     *  with our work.
-     */
-
-    // ----------------------------------- FOLLOW METHOD NAME CONVENTION -----------------------------------------------
-
     @Test
-    void getAllDoctors_SUCCESS() {
+    void getAllDoctors() {
         when(mockDoctorRepository.findAllBy(null))
-                .thenReturn(new PageImpl<>(new ArrayList<>(Arrays.asList(doctor, doctor2))));
+                .thenReturn(new PageImpl<>(new ArrayList<>(Arrays.asList(doctor1, doctor2))));
 
-        Page<Doctor> doctorPage = doctorService.getAllDoctors(null);  // Get doctor page
+        Page<Doctor> doctorPage = doctorService.getAllDoctors(null);
 
-        List<Doctor> doctors = doctorPage.getContent();  // Get list of doctors from doctor page
+        List<Doctor> retrievedDoctors = doctorPage.getContent();
 
         // Test doctor list length
         assertEquals(2, doctors.size());
 
         // Test doctor 1 match
-        assertEquals(doctor, doctors.get(0));
+        assertEquals(doctor1, doctors.get(0));
 
         // Test doctor 2 match
         assertEquals(doctor2, doctors.get(1));
 
+        // Test each doctor matches
+        for (int i = 0; i < doctors.size(); i++) {
+            assertEquals(doctors.get(i), retrievedDoctors.get(i));
+        }
     }
-
-//    @Test
-//    void getAllDoctors_FAIL() {
-//
-//    }
-
-    // ---------------------------- REMOVE THESE LINE COMMENTS AFTER READING -------------------------------------------
 
     @Test
     void getDoctorByID() {
-        when(mockDoctorRepository.findByUserID(doctor.getUserID())).thenReturn(Optional.of(doctor));
-
-        Doctor testDoctor = doctorService.getDoctorByID(doctor.getUserID());
-
-        assertEquals(testDoctor, doctor);
-
+        when(mockDoctorRepository.findByUserID(doctor1.getUserID())).thenReturn(Optional.of(doctor1));
+        assertEquals(doctor1, doctorService.getDoctorByID(doctor1.getUserID()));
     }
-
-    //public Doctor(int userID, String firstName, String lastName, String email, String password,
-    //                  LocalDate dateOfBirth, List<Role> roles, Specialty specialty
 
     @Test
     void updateDoctor() {
+        Doctor updateDoctor = new Doctor(doctor1.getUserID(), "Darrick", "Laidin", "updatedl@gmail.com",
+                "update", dob2 , null, null);
+        updateDoctor.setAccountStatus(false);
 
-        // For reference, BDDMockito has exactly the same functionality as Mockito. The difference lies in that BDDMockito provides methods that are in
-        // more human-readable syntax. Pick whichever you want but be consistent. For more info: https://www.baeldung.com/bdd-mockito
-        BDDMockito.given(mockDoctorRepository.findByUserID(doctor.getUserID())).willReturn(Optional.of(doctor)); //doctor = USERID 1
+        Doctor oldDoctor1 = new Doctor(doctor1.getUserID(), doctor1.getFirstName(), doctor1.getLastName(),
+                doctor1.getEmail(), doctor1.getPassword(), doctor1.getDateOfBirth(), null, null);
 
-        Doctor updateDoctor = new Doctor(-1, "Darrick", "Laidin", "update@gmail.com", "update", dob3 , null, null);
+        when(mockDoctorRepository.findByUserID(doctor1.getUserID())).thenReturn(Optional.of(doctor1));
+        when(mockDoctorRepository.save(doctor1)).thenAnswer(i -> {
+            doctor1.setAccountStatus(updateDoctor.isEnabled());
+            doctor1.setFirstName(updateDoctor.getFirstName());
+            doctor1.setLastName(updateDoctor.getLastName());
+            doctor1.setEmail(updateDoctor.getEmail());
+            doctor1.setPassword(updateDoctor.getPassword());
+            doctor1.setDateOfBirth(updateDoctor.getDateOfBirth());
+            return doctor1;
+        });
 
-        doctorService.updateDoctor(doctor.getUserID(), updateDoctor); //doctor = USERID_1
+        doctorService.updateDoctor(doctor1.getUserID(), updateDoctor);
 
-        assertEquals(1, doctor.getUserID());
-        assertEquals("Darrick", doctor.getFirstName());
-        assertEquals("Laidin", doctor.getLastName());
-        assertEquals("update@gmail.com", doctor.getEmail());
-//        assertEquals("update", doctor.getPassword());
-        assertEquals(dob3,doctor.getDateOfBirth());
-        assertNull(doctor.getRoles());
-        assertNull(doctor.getSpecialty());
+        assertEquals(updateDoctor.getFirstName(), doctor1.getFirstName());
+        assertEquals(updateDoctor.getLastName(), doctor1.getLastName());
+        assertEquals(updateDoctor.getEmail(), doctor1.getEmail());
+        assertEquals(updateDoctor.getPassword(), doctor1.getPassword());
+        assertEquals(updateDoctor.getDateOfBirth(), doctor1.getDateOfBirth());
+
+        assertNotEquals(oldDoctor1.getFirstName(), doctor1.getFirstName());
+        assertNotEquals(oldDoctor1.getLastName(), doctor1.getLastName());
+        assertNotEquals(oldDoctor1.getEmail(), doctor1.getEmail());
+        assertNotEquals(oldDoctor1.getPassword(), doctor1.getPassword());
+        assertNotEquals(oldDoctor1.getDateOfBirth(), doctor1.getDateOfBirth());
+
+        assertNull(doctor1.getRoles());
+        assertNull(doctor1.getSpecialty());
+
     }
 
     @Test
     void deleteDoctor() {
+        when(mockDoctorRepository.findByUserID(doctor1.getUserID())).thenReturn(Optional.of(doctor1));
 
-        when(mockDoctorRepository.findByUserID(doctor.getUserID())).thenReturn(Optional.of(doctor));
+        doNothing().when(mockDoctorRepository).deleteById(doctor1.getUserID());
 
-        doctorService.deleteDoctor(doctor.getUserID());
+        Doctor deletedDoctor = doctorService.deleteDoctor(doctor1.getUserID());
 
-        verify(mockDoctorRepository).deleteById(doctor.getUserID());
+        when(mockDoctorRepository.findAllBy(null))
+                .thenReturn(new PageImpl<>(new ArrayList<>(Collections.singletonList(doctor2))));
+
+        Page<Doctor> doctorPage = doctorService.getAllDoctors(null);
+        List<Doctor> retrievedDoctors = doctorPage.getContent();
+
+        // Test specialty list length
+        assertEquals(doctors.size() - 1, retrievedDoctors.size());
+
+        // Test that specialty1 has been deleted
+        assertEquals(doctor2, retrievedDoctors.get(0));
+
+        // Test that deleted specialty matches specialty1
+        assertEquals(doctor1, deletedDoctor);
     }
 }
