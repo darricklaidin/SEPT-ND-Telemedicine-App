dc4983cb6c8e13aa6f1255e229402ff9739a5cf7
BryanHongRMIT <s3679989@student.rmit.edu.au>
2022-10-17 23:52:46 +1100

    Added PrescriptionUnitControllerTest

---
+++ backend/prescription-microservice/src/test/java/com/sept/prescriptionmicroservice/controller/PrescriptionControllerTest.java
@@ -0,0 +1,41 @@
+package com.sept.prescriptionmicroservice.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrescriptionControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getAllPrescriptions() {
+    }
+
+    @Test
+    void getPrescriptionByID() {
+    }
+
+    @Test
+    void createPrescription() {
+    }
+
+    @Test
+    void getDoctorPrescriptions() {
+    }
+
+    @Test
+    void getPatientPrescriptions() {
+    }
+
+    @Test
+    void updatePrescription() {
+    }
+
+    @Test
+    void deletePrescription() {
+    }
+}
