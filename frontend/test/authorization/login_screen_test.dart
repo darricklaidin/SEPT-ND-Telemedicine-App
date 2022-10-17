917d6d14a3be4252da511eec0f3babde03cc90e1
Nimesh <s3831242@student.rmit.edu.au>
2022-10-17 21:04:37 +1100

    add initial widget tests for login

--- frontend/test/authorization/login_screen_test.dart
+++ frontend/test/authorization/login_screen_test.dart
@@ -1,9 +1,49 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:frontend/modules/authorization/login_screen.dart';
+import 'package:frontend/services/auth_service.dart';
+import 'package:mocktail/mocktail.dart';
 
-// void main() {
-//   testWidgets('Test app', (WidgetTester tester) async {
-//     // Build our app and trigger a frame.
-//     await tester.pumpWidget(LoginScreen());
-//   });
-// }
+class MockAuthService extends Mock implements AuthService {}
+
+void main() {
+  AuthService mockAuthService = MockAuthService();
+
+  Widget createWidgetUnderTest() {
+    return MaterialApp(
+      title: 'SEPT-ND-Telemedicine-App',
+      home: LoginScreen(authService: mockAuthService),
+    );
+  }
+
+  arrangeAuthServiceReturnsNullAuth(bool wait) {
+    when(() => mockAuthService.checkAuth()).thenAnswer((_) async {
+      if (wait) {
+        await Future.delayed(const Duration(seconds: 2));
+      }
+      return null;
+    });
+  }
+
+  testWidgets('title is displayed', (WidgetTester tester) async {
+    // check auth should return null to indicate user is not logged in
+    arrangeAuthServiceReturnsNullAuth(false);
+    await tester.pumpWidget(createWidgetUnderTest());
+    expect(find.text('Login'), findsOneWidget);
+  });
+
+  testWidgets('loading indicator is displayed while checking auth',
+      (WidgetTester tester) async {
+    // check auth should wait 2 seconds and return null
+    arrangeAuthServiceReturnsNullAuth(true);
+    await tester.pumpWidget(createWidgetUnderTest());
+
+    // forces a widget rebuild after 500 milliseconds
+    await tester.pump(const Duration(milliseconds: 500));
+
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+    // waits for no more rebuilds happening (the 2s future)
+    await tester.pumpAndSettle();
+  });
+}
