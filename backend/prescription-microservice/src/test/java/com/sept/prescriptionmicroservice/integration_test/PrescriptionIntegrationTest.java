c85c4a1161e5801208ca7bfba721baf55f9bb87e
BryanHongRMIT <s3679989@student.rmit.edu.au>
2022-10-18 02:06:45 +1100

    completed PrescriptionIntegrationTest

--- backend/prescription-microservice/src/test/java/com/sept/prescriptionmicroservice/integration_test/PrescriptionIntegrationTest.java
+++ backend/prescription-microservice/src/test/java/com/sept/prescriptionmicroservice/integration_test/PrescriptionIntegrationTest.java
@@ -34,45 +34,112 @@ public class PrescriptionIntegrationTest {
 
     @BeforeEach
     void setUp() {
-//        prescriptionRepository.deleteAll(); //clear database
-//
-//        //Add new prescriptions
-//        Prescription prescription1 = new Prescription(1,1,1,"Panadol");
-//
-//        Prescription prescription2 = new Prescription(2,2,2,"Telfast");
-//
-//        Prescription prescription3 = new Prescription(3,2,1,"Zyrtec");
-//
-//        Prescription prescription = prescriptionRepository.findAllBy(null).getContent().get(0);
-//
-//        prescriptionService.createPrescription("")
+        prescriptionRepository.deleteAll(); //clear database
+
+        //Add new prescriptions
+        Prescription prescription1 = new Prescription(1,2,1,"Panadol");
+
+        Prescription prescription2 = new Prescription(2,4,3,"Telfast");
+
+        Prescription prescription3 = new Prescription(3,2,3,"Zyrtec");
+
+        Prescription prescription4 = new Prescription(3,2,1,"Tester");
+
+        prescriptionRepository.save(prescription1);
+        prescriptionRepository.save(prescription2);
+        prescriptionRepository.save(prescription3);
+        prescriptionRepository.save(prescription4);
+
+        Prescription prescription = prescriptionRepository.findAllBy(null).getContent().get(0);
+
     }
 
     @Test
-    void getPrescriptionByID() {
+    void getPrescriptionByID() throws Exception {
+        Prescription prescription = prescriptionRepository.findAllBy(null).getContent().get(0);
+
+        mockMvc.perform(get("/api/prescriptions/" + prescription.getPrescriptionID()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.prescription").value(prescription.getPrescription()))
+                .andExpect(jsonPath("$.patientID").value(1))
+                .andExpect(jsonPath("$.doctorID").value(2));
+    }
+
+
+    @Test
+    void testGetPrescriptionByIdNotFound() throws Exception {
+        mockMvc.perform(get("/api/prescriptions/-1"))
+                .andExpect(status().isNotFound());
     }
 
     @Test
-    void getAllPrescriptions() {
+    void getAllPrescriptions() throws Exception {
+        mockMvc.perform(get("/api/prescriptions"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$['content']", hasSize(4)))
+                .andExpect(jsonPath("$['content'][0].prescription").value("Panadol"))
+                .andExpect(jsonPath("$['content'][1].prescription").value("Telfast"))
+                .andExpect(jsonPath("$['content'][2].prescription").value("Zyrtec"))
+                .andExpect(jsonPath("$['content'][3].prescription").value("Tester"));
+
+    }
+
+
+    @Test
+    void getPatientPrescriptions() throws Exception {
+        Prescription prescription = prescriptionRepository.findAllBy(null).getContent().get(0);
+
+        mockMvc.perform(get("/api/prescriptions/patient/"+prescription.getPatientID()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$['content']", hasSize(2)))
+                .andExpect(jsonPath("$['content'][0].prescription").value("Panadol"))
+                .andExpect(jsonPath("$['content'][1].prescription").value("Tester"));
     }
 
     @Test
-    void getPatientPrescriptions() {
+    void getDoctorPrescriptions() throws Exception {
+        Prescription prescription = prescriptionRepository.findAllBy(null).getContent().get(0);
+
+        mockMvc.perform(get("/api/prescriptions/doctor/"+prescription.getDoctorID()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$['content']", hasSize(3)))
+                .andExpect(jsonPath("$['content'][0].prescription").value("Panadol"))
+                .andExpect(jsonPath("$['content'][1].prescription").value("Zyrtec"))
+                .andExpect(jsonPath("$['content'][2].prescription").value("Tester"));
     }
 
     @Test
-    void getDoctorPrescriptions() {
+    void createPrescription() throws Exception {
+        mockMvc.perform(post("/api/prescriptions")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{ \"prescription\": \"Cough Med\" }")
+//                        .content("{ \"patientID\": 3 }")
+//                        .content("{ \"doctorID\": 2 }")
+                )
+
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.prescription").value("Cough Med"));
+//                .andExpect(jsonPath("$.patientID").value("1"));
     }
 
     @Test
-    void createPrescription() {
+    void updatePrescription() throws Exception {
+        Prescription prescription = prescriptionRepository.findAllBy(null).getContent().get(0);
+
+        mockMvc.perform(put("/api/prescriptions/" + prescription.getPrescriptionID())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{ \"prescription\": \"Cough\" }"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.prescription").value("Cough"));
     }
 
     @Test
-    void updatePrescription() {
-    }
+    void deletePrescription() throws Exception {
+        Prescription prescription = prescriptionRepository.findAllBy(null).getContent().get(0);
 
-    @Test
-    void deletePrescription() {
+        mockMvc.perform(delete("/api/prescriptions/" + prescription.getPrescriptionID()))
+                .andExpect(status().isOk());
+
+        assertTrue(prescriptionRepository.findByPrescriptionID(prescription.getPrescriptionID()).isEmpty());
     }
 }
