6fdde78dbf400137220edae26355d23e936acc10
BryanHongRMIT <s3679989@student.rmit.edu.au>
2022-10-17 15:33:16 +1100

    added prescription screen button to profile_screen.dart

--- frontend/lib/modules/profile/profile_screen.dart
+++ frontend/lib/modules/profile/profile_screen.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:age_calculator/age_calculator.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:frontend/modules/prescription/prescription_screen.dart';
 import 'package:frontend/modules/profile/profile_button.dart';
 import 'package:frontend/services/doctor_service.dart';
 import 'package:frontend/utility.dart';
@@ -137,6 +138,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                       ..._buildAvailabilitySchedule(width, height, primaryThemeColor, secondaryThemeColor, errorThemeColor),
                       SizedBox(height: height * 0.05),
                       ..._buildBookAppointmentBtn(width, height, primaryThemeColor, secondaryThemeColor, errorThemeColor),
+                      ..._buildPrescriptionBtn(width, height, primaryThemeColor, secondaryThemeColor, errorThemeColor),
                     ],
                   ),
                 ),
@@ -276,6 +278,44 @@ class _ProfileScreenState extends State<ProfileScreen> {
             ),
             child: const Text(
               'Book Appointment',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      ),
+      SizedBox(
+        height: height * 0.05,
+      )
+    ];
+  }
+
+  List<Widget> _buildPrescriptionBtn(double width, double height, Color primaryThemeColor, Color secondaryThemeColor, Color errorThemeColor) {
+    return widget.userRole == "DOCTOR" ? [SizedBox(height: height * 0.05,)] : [
+      Center(
+        child: SizedBox(
+          width: width * 0.45,
+          height: 40,
+          child: TextButton(
+            // Navigate to make appointment page
+            onPressed: () async {
+              if (await checkIfUserExists(errorThemeColor)) {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => PrescriptionScreen(doctor: widget.user)));
+              }
+            },
+            style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.all(secondaryThemeColor),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+              ),
+            ),
+            child: const Text(
+              'Prescribe Medicine',
               style: TextStyle(
                 color: Colors.white,
                 fontSize: 14,
