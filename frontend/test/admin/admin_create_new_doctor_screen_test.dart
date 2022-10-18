ceb045ccb90c4eefda6c83c0bdcf3374a0fde296
Nimesh <s3831242@student.rmit.edu.au>
2022-10-18 22:42:20 +1100

    fix

--- frontend/test/admin/admin_create_new_doctor_screen_test.dart
+++ frontend/test/admin/admin_create_new_doctor_screen_test.dart
@@ -1,182 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:frontend/models/api_response.dart';
-import 'package:frontend/modules/authorization/login_screen.dart';
-import 'package:frontend/modules/home/main_screen.dart';
+import 'package:frontend/models/specialty.dart';
+import 'package:frontend/modules/admin/admin_create_new_doctor_screen.dart';
 import 'package:frontend/services/auth_service.dart';
+import 'package:frontend/services/specialty_service.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockAuthService extends Mock implements AuthService {}
+
+class MockSpecialtyService extends Mock implements SpecialtyService {}
 
 class MockNavigatorObserver extends Mock implements NavigatorObserver {}
 
 void main() {
   late AuthService mockAuthService;
+  late MockSpecialtyService mockSpecialtyService;
   late NavigatorObserver mockObserver;
+  Specialty mockSpecialty = Specialty(specialtyID: 1, specialtyName: "GP");
 
   setUp(() {
     mockAuthService = MockAuthService();
+    mockSpecialtyService = MockSpecialtyService();
     mockObserver = MockNavigatorObserver();
   });
+
+  arrangeSpecialtyServiceReturnsSpecialties() {
+    when(() => mockSpecialtyService.getSpecialties())
+        .thenAnswer((_) async => [mockSpecialty]);
+  }
+
+  arrangeAuthServiceRegisterDoctor(String firstName, String lastName,
+      String email, String password, String dateOfBirth, int specialtyID) {
+    when(() => mockAuthService.registerDoctor(
+            firstName, lastName, email, password, dateOfBirth, specialtyID))
+        .thenAnswer((_) async {
+      ApiResponse res = ApiResponse();
+      res.success = true;
+      return res;
+    });
+  }
 
   Widget createWidgetUnderTest() {
     return MaterialApp(
       title: 'SEPT-ND-Telemedicine-App',
-      home: LoginScreen(authService: mockAuthService),
-      // This mocked observer will now receive all navigation events
-      // that happen in our app.
+      home: AdminCreateNewDoctorScreen(
+        authService: mockAuthService,
+        specialtyService: SpecialtyService(),
+      ),
       navigatorObservers: [mockObserver],
-      routes: {
-        "/home": (context) => const MainScreen(),
-      },
     );
-  }
-
-  arrangeAuthServiceReturnsNullAuth(bool wait) {
-    when(() => mockAuthService.checkAuth()).thenAnswer((_) async {
-      if (wait) {
-        await Future.delayed(const Duration(seconds: 2));
-      }
-      return null;
-    });
-  }
-
-  arrangeAuthServiceReturnsLoginSuccess(
-      String email, String password, bool res) {
-    when(() => mockAuthService.loginUser(email, password))
-        .thenAnswer((_) async {
-      ApiResponse apiRespone = ApiResponse();
-      apiRespone.success = res;
-      return apiRespone;
-    });
-  }
-
-  arrangeAuthServiceReturnsRole(String role) {
-    when(() => mockAuthService.getUserRoleFromStorage())
-        .thenAnswer((_) async => role);
   }
 
   testWidgets('title is displayed', (WidgetTester tester) async {
     // check auth should return null to indicate user is not logged in
-    arrangeAuthServiceReturnsNullAuth(false);
     await tester.pumpWidget(createWidgetUnderTest());
-    expect(find.text('Login'), findsOneWidget);
-  });
-
-  testWidgets('loading indicator is displayed while checking auth',
-      (WidgetTester tester) async {
-    // check auth should wait 2 seconds and return null
-    arrangeAuthServiceReturnsNullAuth(true);
-    await tester.pumpWidget(createWidgetUnderTest());
-
-    // forces a widget rebuild after 500 milliseconds
-    await tester.pump(const Duration(milliseconds: 500));
-
-    expect(find.byType(CircularProgressIndicator), findsOneWidget);
-
-    // waits for no more rebuilds happening (the 2s future)
-    await tester.pumpAndSettle();
-  });
-
-  testWidgets('empty email and password validaton',
-      (WidgetTester tester) async {
-    // load screen
-    arrangeAuthServiceReturnsNullAuth(false);
-    await tester.pumpWidget(createWidgetUnderTest());
-    await tester.pump();
-
-    // test login button click with empty input
-    await tester.tap(find.byType(ElevatedButton));
-    await tester.pumpAndSettle();
-
-    // expect validator error messages
-    expect(find.text('Email cannot be empty'), findsOneWidget);
-    expect(find.text('Password cannot be empty'), findsOneWidget);
-  });
-
-  testWidgets('invalid email and password validaton',
-      (WidgetTester tester) async {
-    // load screen
-    arrangeAuthServiceReturnsNullAuth(false);
-    await tester.pumpWidget(createWidgetUnderTest());
-    await tester.pump();
-
-    // test login button click with invalid input
-    String invalidEmail = "invalidemail.com"; // invalid email syntax
-    String invalidPassword = "123"; // too short
-    Finder email = find.byKey(const Key('email'));
-    Finder pwd = find.byKey(const Key('password'));
-
-    await tester.enterText(email, invalidEmail);
-    await tester.enterText(pwd, invalidPassword);
-    await tester.tap(find.byType(ElevatedButton));
-    await tester.pumpAndSettle();
-
-    // expect validator error messages
-    expect(find.text('Invalid email'), findsOneWidget);
-    expect(
-        find.text('Password should be atleast 8 characters'), findsOneWidget);
-  });
-
-  testWidgets(
-      'valid email and password, valid account, call loginUser, succeed',
-      (WidgetTester tester) async {
-    String validEmail = "email@email.com";
-    String validPassword = "123456textemptynot";
-
-    // load screen with required auth returns
-    arrangeAuthServiceReturnsNullAuth(false);
-    arrangeAuthServiceReturnsRole('USER');
-    arrangeAuthServiceReturnsLoginSuccess(validEmail, validPassword, true);
-    await tester.pumpWidget(createWidgetUnderTest());
-    await tester.pump();
-
-    // test login button click with non-empty input
-    Finder email = find.byKey(const Key('email'));
-    Finder pwd = find.byKey(const Key('password'));
-
-    await tester.enterText(email, validEmail);
-    await tester.enterText(pwd, validPassword);
-    await tester.tap(find.byType(ElevatedButton));
-    await tester.pump();
-
-    // expect loginUser being called and success snackbar shown
-    verify(() => mockAuthService.loginUser(validEmail, validPassword))
-        .called(1);
-    expect(find.text('Login Successful'), findsOneWidget);
-
-    /// Verify that a push event happened
-    verify(
-      () => mockObserver.didReplace(
-        oldRoute: any(named: 'oldRoute'),
-        newRoute: any(named: 'newRoute'),
-      ),
-    );
-  });
-
-  testWidgets('valid email and password, valid account, call loginUser, fail',
-      (WidgetTester tester) async {
-    String validEmail = "email@email.com";
-    String validPassword = "123456textemptynot";
-
-    // load screen with required auth returns
-    arrangeAuthServiceReturnsNullAuth(false);
-    arrangeAuthServiceReturnsLoginSuccess(validEmail, validPassword, false);
-    await tester.pumpWidget(createWidgetUnderTest());
-    await tester.pump();
-
-    // test login button click with non-empty input
-    Finder email = find.byKey(const Key('email'));
-    Finder pwd = find.byKey(const Key('password'));
-
-    await tester.enterText(email, validEmail);
-    await tester.enterText(pwd, validPassword);
-    await tester.tap(find.byType(ElevatedButton));
-    await tester.pump();
-
-    // expect loginUser being called and failure snackbar shown
-    verify(() => mockAuthService.loginUser(validEmail, validPassword))
-        .called(1);
-    expect(find.text('Invalid Credentials'), findsOneWidget);
+    expect(find.text('Register New Doctor'), findsOneWidget);
   });
 }
--- frontend/test/appointment/manage_appointments_screen_test.dart
+++ frontend/test/appointment/manage_appointments_screen_test.dart
@@ -1,12 +1,15 @@
+import 'package:age_calculator/age_calculator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:frontend/models/api_response.dart';
-import 'package:frontend/models/specialty.dart';
-import 'package:frontend/modules/admin/admin_create_new_doctor_screen.dart';
+import 'package:frontend/data/appointments.dart';
+import 'package:frontend/models/appointment.dart';
+import 'package:frontend/modules/appointment/manage_appointments_screen.dart';
 import 'package:frontend/services/auth_service.dart';
 import 'package:frontend/services/doctor_service.dart';
 import 'package:frontend/services/patient_service.dart';
 import 'package:frontend/services/specialty_service.dart';
+import 'package:frontend/utility.dart';
+import 'package:intl/intl.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockAuthService extends Mock implements AuthService {}
@@ -15,34 +18,45 @@ class MockPatientService extends Mock implements PatientService {}
 
 class MockDoctorService extends Mock implements DoctorService {}
 
-class MockSpecialtyService extends Mock implements SpecialtyService {}
-
 class MockNavigatorObserver extends Mock implements NavigatorObserver {}
 
 void main() {
   late AuthService mockAuthService;
-  late MockSpecialtyService mockSpecialtyService;
+  late MockPatientService mockPatientService;
+  late MockDoctorService mockDoctorService;
   late NavigatorObserver mockObserver;
-  Specialty mockSpecialty = Specialty(specialtyID: 1, specialtyName: "GP");
 
   setUp(() {
     mockAuthService = MockAuthService();
-    mockSpecialtyService = MockSpecialtyService();
+    mockPatientService = MockPatientService();
+    mockDoctorService = MockDoctorService();
     mockObserver = MockNavigatorObserver();
   });
 
-  arrangeSpecialtyServiceReturnsSpecialties() {
-    when(() => mockSpecialtyService.getSpecialties())
-        .thenAnswer((_) async => [mockSpecialty]);
+  arrangeAuthServiceReturnsUserId(int id) {
+    when(() => mockAuthService.getUserIdFromStorage())
+        .thenAnswer((_) async => id);
   }
 
-  arrangeAuthServiceRegisterDoctor(String firstName, String lastName,
-      String email, String password, String dateOfBirth, int specialtyID) {
-    when(() => mockAuthService.registerDoctor(
-            firstName, lastName, email, password, dateOfBirth, specialtyID))
+  arrangeAuthServiceReturnsRole(String role) {
+    when(() => mockAuthService.getUserRoleFromStorage())
+        .thenAnswer((_) async => role);
+  }
+
+  arrangePatientServiceReturnsAppointments(List<Appointment> res, int id,
+      {bool wait = false}) {
+    when(() => mockPatientService.fetchPatientAppointments(id))
         .thenAnswer((_) async {
-      ApiResponse res = ApiResponse();
-      res.success = true;
+      if (wait) {
+        await Future.delayed(const Duration(seconds: 2));
+      }
+      return res;
+    });
+  }
+
+  arrangeDoctorServiceReturnsAppointments(List<Appointment> res, int id) {
+    when(() => mockDoctorService.fetchDoctorAppointments(id))
+        .thenAnswer((_) async {
       return res;
     });
   }
@@ -50,8 +64,11 @@ void main() {
   Widget createWidgetUnderTest() {
     return MaterialApp(
       title: 'SEPT-ND-Telemedicine-App',
-      home: AdminCreateNewDoctorScreen(
+      home: ManageAppointmentsScreen(
+        handleTabSelection: () => {},
         authService: mockAuthService,
+        patientService: mockPatientService,
+        doctorService: mockDoctorService,
         specialtyService: SpecialtyService(),
       ),
       navigatorObservers: [mockObserver],
@@ -60,7 +77,95 @@ void main() {
 
   testWidgets('title is displayed', (WidgetTester tester) async {
     // check auth should return null to indicate user is not logged in
+    arrangeAuthServiceReturnsUserId(1);
+    arrangeAuthServiceReturnsRole("PATIENT");
+    arrangePatientServiceReturnsAppointments([], 1);
     await tester.pumpWidget(createWidgetUnderTest());
-    expect(find.text('Register New Doctor'), findsOneWidget);
+    expect(find.text('Appointments'), findsOneWidget);
   });
+
+  testWidgets('loading indicator is displayed while fetching appointments',
+      (WidgetTester tester) async {
+    // fetch should wait 2 seconds and return null
+    arrangeAuthServiceReturnsUserId(1);
+    arrangeAuthServiceReturnsRole("PATIENT");
+    arrangePatientServiceReturnsAppointments([], 1, wait: true);
+    await tester.pumpWidget(createWidgetUnderTest());
+
+    // forces a widget rebuild after 500 milliseconds
+    await tester.pump(const Duration(milliseconds: 500));
+
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+    // waits for no more rebuilds happening (the 2s future)
+    await tester.pumpAndSettle();
+  });
+
+  testWidgets(
+    "appointments are displayed for patient",
+    (WidgetTester tester) async {
+      arrangeAuthServiceReturnsUserId(2);
+      arrangeAuthServiceReturnsRole("PATIENT");
+      arrangePatientServiceReturnsAppointments(mockAppointments, 2);
+
+      await tester.pumpWidget(createWidgetUnderTest());
+
+      await tester.pump();
+
+      for (final appointment in mockAppointments) {
+        expect(
+            find.text(
+                "${appointment.doctor.firstName} ${appointment.doctor.lastName}"),
+            findsOneWidget);
+        expect(
+            find.text(
+                "Age: ${AgeCalculator.age(appointment.doctor.dateOfBirth).years}"),
+            findsOneWidget);
+        expect(
+            find.text(
+              DateFormat('dd MMM yyyy').format(appointment.date),
+            ),
+            findsOneWidget);
+        expect(
+            find.text(
+              '${Utility.timeToString(appointment.startTime)} - ${Utility.timeToString(appointment.endTime)}',
+            ),
+            findsOneWidget);
+      }
+    },
+  );
+
+  testWidgets(
+    "appointments are displayed for doctor",
+    (WidgetTester tester) async {
+      arrangeAuthServiceReturnsUserId(1);
+      arrangeAuthServiceReturnsRole("DOCTOR");
+      arrangeDoctorServiceReturnsAppointments(mockAppointments, 1);
+
+      await tester.pumpWidget(createWidgetUnderTest());
+
+      await tester.pump();
+
+      for (final appointment in mockAppointments) {
+        expect(
+            find.text(
+                "${appointment.patient.firstName} ${appointment.patient.lastName}"),
+            findsOneWidget);
+        expect(
+            find.text(
+                "Age: ${AgeCalculator.age(appointment.patient.dateOfBirth).years}"),
+            findsOneWidget);
+        expect(
+            find.text(
+              DateFormat('dd MMM yyyy').format(appointment.date),
+            ),
+            findsOneWidget);
+        expect(
+            find.text(
+              '${Utility.timeToString(appointment.startTime)} - ${Utility.timeToString(appointment.endTime)}',
+            ),
+            findsOneWidget);
+      }
+    },
+  );
 }
