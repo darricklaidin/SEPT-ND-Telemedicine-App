8090e58cdcae1de9a70f24d7a37fdab3369ef7a2
Nimesh <s3831242@student.rmit.edu.au>
2022-10-18 22:28:55 +1100

    fix renderflow issue in tests

--- frontend/lib/modules/admin/admin_create_new_doctor_screen.dart
+++ frontend/lib/modules/admin/admin_create_new_doctor_screen.dart
@@ -186,43 +186,39 @@ class _AdminCreateNewDoctorScreenState
                   const SizedBox(height: 20),
                   Align(
                     alignment: Alignment.center,
-                    child: Container(
-                        width: 300,
-                        padding: EdgeInsets.symmetric(horizontal: width * 0.05),
-                        child: Row(
-                          children: [
-                            const Text("Date of Birth:"),
-                            SizedBox(
-                              width: width * 0.15,
-                            ),
-                            ElevatedButton(
-                              onPressed: () async {
-                                DateTime? pickedDate = await showDatePicker(
-                                  context: context,
-                                  initialDate: DateTime.now(),
-                                  firstDate: DateTime(1900),
-                                  //DateTime.now() - not to allow to choose before today.
-                                  lastDate: DateTime.now(),
-                                );
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("Date of Birth:"),
+                        SizedBox(
+                          width: width * 0.15,
+                        ),
+                        ElevatedButton(
+                          onPressed: () async {
+                            DateTime? pickedDate = await showDatePicker(
+                              context: context,
+                              initialDate: DateTime.now(),
+                              firstDate: DateTime(1900),
+                              //DateTime.now() - not to allow to choose before today.
+                              lastDate: DateTime.now(),
+                            );
 
-                                if (pickedDate != null) {
-                                  String formattedDate =
-                                      DateFormat('yyyy-MM-dd')
-                                          .format(pickedDate);
-                                  setState(() {
-                                    dateInput =
-                                        formattedDate; //set output date to TextField value.
-                                  });
-                                }
-                              },
-                              child: Text(
-                                dateInput ?? "Select Date",
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                          ],
-                        )),
+                            if (pickedDate != null) {
+                              String formattedDate =
+                                  DateFormat('yyyy-MM-dd').format(pickedDate);
+                              setState(() {
+                                dateInput =
+                                    formattedDate; //set output date to TextField value.
+                              });
+                            }
+                          },
+                          child: Text(
+                            dateInput ?? "Select Date",
+                            style: const TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                   const SizedBox(height: 20),
                   Align(
